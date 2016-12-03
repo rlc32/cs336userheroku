134fe46b7a642929e6cbab71b587b22786562146
kvlinden <rlc32@students.calvin.edu>
2016-12-03 14:08:03 -0500

    adding all back

---
+++ README.md
@@ -0,0 +1,4 @@
+# CS336 final project
+# Mark Davis & Russell Clousing
+
+Our goal is to take the Washington Post database of police shootings into a Mongo database. Then, a ReactJS frontend will display the data in an organized fashion.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack')
+module.exports = {
+    entry: [
+        __dirname + '/app/scripts/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: '/bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.css$/,  loader: 'style!css?modules!postcss' }
+        ]
+    },
+    plugins: [
+    new HtmlWebpackPlugin({template: __dirname + "/app/index.tmpl.html"}),
+    new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        port: 3001,
+        proxy: { '/api/*': 'http://localhost:3000' },
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        hot: true
+    }
+};
